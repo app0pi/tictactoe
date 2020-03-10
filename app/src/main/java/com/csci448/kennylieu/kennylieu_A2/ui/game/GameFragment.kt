d28d7138b7d1ce5f07d16f561647ece1e216540d
app0pi <kenny.lieu@yahoo.com>
2020-03-09 20:13:57 -0600

    set on click for all 9 blocks and reset

--- app/src/main/java/com/csci448/kennylieu/kennylieu_A2/ui/game/GameFragment.kt
+++ app/src/main/java/com/csci448/kennylieu/kennylieu_A2/ui/game/GameFragment.kt
@@ -28,7 +28,26 @@ class GameFragment : Fragment() {
 
     private lateinit var playAgainButton: Button
     private lateinit var goBackButton: Button
+
     private lateinit var block1: ImageView
+    private lateinit var block2: ImageView
+    private lateinit var block3: ImageView
+    private lateinit var block4: ImageView
+    private lateinit var block5: ImageView
+    private lateinit var block6: ImageView
+    private lateinit var block7: ImageView
+    private lateinit var block8: ImageView
+    private lateinit var block9: ImageView
+
+
+    private var currentPiece: Int = R.drawable.ic_o
+
+    fun switchPiece(){
+        if(currentPiece == R.drawable.ic_o)
+            currentPiece = R.drawable.ic_x
+        else
+            currentPiece = R.drawable.ic_o
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,17 +58,68 @@ class GameFragment : Fragment() {
         val view = inflater.inflate(R.layout.game_fragment, container, false)
         playAgainButton = view?.findViewById(R.id.play_again_button) as Button
         goBackButton = view?.findViewById(R.id.go_back_button) as Button
+        block1 = view?.findViewById(R.id.block1) as ImageView
+        block2 = view?.findViewById(R.id.block2) as ImageView
+        block3 = view?.findViewById(R.id.block3) as ImageView
+        block4 = view?.findViewById(R.id.block4) as ImageView
+        block5 = view?.findViewById(R.id.block5) as ImageView
+        block6 = view?.findViewById(R.id.block6) as ImageView
+        block7 = view?.findViewById(R.id.block7) as ImageView
+        block8 = view?.findViewById(R.id.block8) as ImageView
+        block9 = view?.findViewById(R.id.block9) as ImageView
+
         playAgainButton.setOnClickListener {
-            //TODO set play again button action
+            block1.setImageResource(android.R.color.transparent)
+            block2.setImageResource(android.R.color.transparent)
+            block3.setImageResource(android.R.color.transparent)
+            block4.setImageResource(android.R.color.transparent)
+            block5.setImageResource(android.R.color.transparent)
+            block6.setImageResource(android.R.color.transparent)
+            block7.setImageResource(android.R.color.transparent)
+            block8.setImageResource(android.R.color.transparent)
+            block9.setImageResource(android.R.color.transparent)
         }
         goBackButton.setOnClickListener {
             callbacks?.onGameBackClicked()
         }
-        block1 = view?.findViewById(R.id.block1) as ImageView
+
         block1.setOnClickListener {
-            block1.setImageResource(R.drawable.ic_o)
-            //block1.setImageResource(android.R.color.transparent)
+            block1.setImageResource(currentPiece)
+            switchPiece()
         }
+        block2.setOnClickListener {
+            block2.setImageResource(currentPiece)
+            switchPiece()
+        }
+        block3.setOnClickListener {
+            block3.setImageResource(currentPiece)
+            switchPiece()
+        }
+        block4.setOnClickListener {
+            block4.setImageResource(currentPiece)
+            switchPiece()
+        }
+        block5.setOnClickListener {
+            block5.setImageResource(currentPiece)
+            switchPiece()
+        }
+        block6.setOnClickListener {
+            block6.setImageResource(currentPiece)
+            switchPiece()
+        }
+        block7.setOnClickListener {
+            block7.setImageResource(currentPiece)
+            switchPiece()
+        }
+        block8.setOnClickListener {
+            block8.setImageResource(currentPiece)
+            switchPiece()
+        }
+        block9.setOnClickListener {
+            block9.setImageResource(currentPiece)
+            switchPiece()
+        }
+
         return view
 
     }
